33e9cfaee69ceaba375a97d1e405e93679a612de
lemarc7k <mvkaworld@gmail.com>
2025-07-29 12:25:59 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function generate() {
-    const input = document.getElementById('input').value;
-    const output = document.getElementById('output');
-    if (!input.trim()) {
-        output.innerHTML = "<p>Por favor escribe algo primero.</p>";
-        return;
-    }
-    output.innerHTML = "<h2>Tu Carta</h2><p>" + input.replace(/\n/g, "<br>") + "</p>";
-}
